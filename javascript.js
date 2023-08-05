fef1359058c114946a68cfd8d534ff1a928989ba
Lucho <90161223+LucianoAlexisBarboza@users.noreply.github.com>
2023-08-05 03:21:42 -0300

    Add reset button

--- javascript.js
+++ javascript.js
@@ -1,18 +1,16 @@
 const container = document.querySelector(".container");
 rows();
-const gridColumn = document.querySelectorAll(".rows");
-column();
-const grid = document.querySelectorAll(".columns");
 
 function rows() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
     for(let i = 0; i < 16; i++) {
         const content = document.createElement("div");
         container.appendChild(content);
         content.classList.add("rows");     
     }
-}
-
-function column() {
+    const gridColumn = document.querySelectorAll(".rows");
     gridColumn.forEach(function(column) {
         for(let i = 0; i < 16; i++) {
             const content = document.createElement("div");
@@ -22,8 +20,12 @@ function column() {
     });
 }
 
+const reset = document.querySelector("#eraser")
+reset.addEventListener('click', rows);
+
+const grid = document.querySelectorAll(".columns");
 grid.forEach((columns) => {
     columns.addEventListener('click', () => {
       columns.style.background = "black";
     });
-  });
+  });   
