e4c4fb9dd024e12a03835bf5e5137fb9a152f449
Lucho <90161223+LucianoAlexisBarboza@users.noreply.github.com>
2023-08-07 17:59:02 -0300

    Add new grid button

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
 const container = document.querySelector(".container");
 const gridSize = parseInt(prompt("Choose square size"));
 
-function grid(size) {
-    for(let i = 0; i < size; i++) {
+function grid(gridSize = 16) {
+    for(let i = 0; i < gridSize; i++) {
         let column = document.createElement("div");
         column.classList.add("column");
-    for(let i = 1; i <= size; i++) {
+    for(let i = 1; i <= gridSize; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
         column.appendChild(row);
@@ -30,3 +30,8 @@ btn.addEventListener("click", () => {
         row.style.backgroundColor = "white";
     });
 });
+
+const refresh = document.querySelector("#btn1");
+refresh.addEventListener("click", () => {
+    location.reload();
+})
